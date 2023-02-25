71bcb53747763576e9bd41f9d882b4d660d90725
Denis Jeronimo Viana Filho <denisviana@gmail.com>
2023-02-25 19:10:11 -0300

    Initial commit

---
+++ Aula02/CursoJava.java
@@ -0,0 +1,11 @@
+public class CursoJava{
+    public static void main(String[] args){
+
+        int num=0;
+
+        System.out.print("Denis Viana Cursos\n");
+        System.out.println("Curso de Java");
+        System.out.printf("Canal:%s%nAno:%d", "Denis Viana Cursos", 2023);
+        System.out.printf("%nValor da variável num:%d", num);
+    }
+}
