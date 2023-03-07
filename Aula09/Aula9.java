88f3704326bb0ffcd1013736470359636b2556da
Denis Jeronimo Viana Filho <denisviana@gmail.com>
2023-03-07 09:16:48 -0300

    matrizes e metodos

---
+++ Aula09/Aula9.java
@@ -0,0 +1,10 @@
+public class Aula9{
+    public static void main(String[] args){
+        for(int i=0;i<10;i++){
+            canal();
+        }
+    }
+    public static void canal(){
+        System.out.println("Denis Viana - Cursos");
+    }
+}
---
+++ Aula09/Aula9B.java
@@ -0,0 +1,11 @@
+public class Aula9B{
+    public static void main(String[] args){
+        
+        msg("Denis Viana");
+        msg("Cursos e treinamentos");
+        msg("Online");
+    }
+    public static void msg(String m){
+        System.out.println(m);
+    }
+}
